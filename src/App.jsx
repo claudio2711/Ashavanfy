1ddcf9f4f4b2ac3cad24a3a871dc9c24f07a2387
Claudio Di Salvo <claudio27-11@hotmail.com>
2025-07-01 15:51:27 +0200

    aggiunti sidebar, player, display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 //rafce
-import React from "react"
 import Sidebar from "./components/Sidebar"
+import Player from "./components/Player"
+import Display from "./components/Display"
 
 const App = () => {
   return (
-    <div className="h-screen bg-[#010701]">
+    <div className="h-screen bg-[#29053d]">
       <div className="h-[90%] flex">
         <Sidebar  />
+        <Display/>
       </div>
+       <Player/>
     </div>
   )
 }
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -27,14 +27,14 @@ import img2 from './img2.jpg'
 import img3 from './img3.jpg'
 import img4 from './img4.jpg'
 import img5 from './img5.jpg'
-import img6 from './img6.jpg'
+//import img6 from './img6.jpg'
 import img7 from './img7.jpg'
 import img8 from './img8.jpg'
 import img9 from './img9.jpg'
 import img10 from './img10.jpg'
 import img11 from './img11.jpg'
 import img12 from './img12.jpg'
-import img13 from './img13.jpg'
+//import img13 from './img13.jpg'
 import img14 from './img14.jpg'
 import img15 from './img15.jpg'
 import img16 from './img16.jpg'
@@ -117,27 +117,27 @@ export const albumsData = [
 export const songsData = [
     {
         id:0,
-        name: "Song One",
+        name: "Moero, Marisa",
         image: img1,
         file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"3:00"
+        desc:"Let them all burn in hell",
+        duration:"4:04"
     },
     {
         id:1,
-        name: "Song Two",
+        name: "Claw of vengeance",
         image: img2,
         file:song2,
-        desc:"Put a smile on your face with these happy tunes",
+        desc:"The forgotten name from stars",
         duration:"2:20"
     },
     {
         id:2,
-        name: "Song Three",
+        name: "Madness bound in infinity",
         image: img3,
         file:song3,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:32"
+        desc:"The mark of emptiness",
+        duration:"3:32"
     },
     {
         id:3,
